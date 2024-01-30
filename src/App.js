fa117cdb7ec043fef371a029ba5609c823a17f00
Lucas Silva <lucasstrlisilva@outlook.com>
2024-01-29 22:09:27 -0300

    initial web application react for study

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Test
         </p>
         <a
           className="App-link"
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Teste React
         </a>
       </header>
     </div>

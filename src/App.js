db85ac5f9dd5e0f28613813b6efe00cf6d36f69d
Lucas Silva <lucasstrlisilva@outlook.com>
2024-03-25 21:18:21 -0300

    add option for change secondary and primary color of the employee

--- src/App.js
+++ src/App.js
@@ -4,38 +4,32 @@ import Form from "./components/Form";
 import Teams from "./components/Teams";
 
 function App() {
-  const teams = [
+  const [teams, setTeams] = useState ([
     {
       name: 'Programação',
-      primaryColor: '#57C278',
-      secondaryColor: '#D9F7E9',
+      color: '#D9F7E9',
     },
     {
       name: 'Front-End',
-      primaryColor: '#82CFFA',
-      secondaryColor: '#E8F8FF',
+      color: '#E8F8FF',
     },
     {
       name: 'Data Science',
-      primaryColor: '#A6D157',
-      secondaryColor: '#F8F8E2',
+      color: '#F8F8E2',
     },
     {
       name: 'Devops',
-      primaryColor: '#E06869',
-      secondaryColor: '#FAE9F5',
+      color: '#FAE9F5',
     },
     {
       name: 'UX e Design',
-      primaryColor: '#FFBA05',
-      secondaryColor: '#FFF5D9',
+      color: '#FFF5D9',
     },
     {
       name: "Inovação e Gestão",
-      primaryColor: "#FF8A29",
-      secondaryColor: "#FFEEDF",
+      color: "#FFEEDF",
     },
-  ];
+  ]);
   const [employees, setEmployees] = useState([]);
   const onNewEmployee = (employee) => {
     setEmployees([...employees, employee]);
@@ -45,16 +39,25 @@ function App() {
     console.log('deletando colaborador');
   };
 
+  const changeColorTeam = (color, nameTeam) => {
+    setTeams(teams.map(team => {
+      if(team.name === nameTeam){
+        team.color = color;
+      }
+      return team;
+    }))
+  }
+
   return (
     <div className="App">
       <Banner />
       <Form teams={teams.map(team => team.name)} onEmployeeRegister={(employee) => onNewEmployee(employee)} />
       {teams.map(team => 
         <Teams
+          changeColor={changeColorTeam}
           key={team.name}
           name={team.name}
-          primaryColor={team.primaryColor}
-          secondaryColor={team.secondaryColor}
+          team={team}
           employees={employees.filter(e => e.team === team.name)}
           onDeleting={deleteEmployee}
         />
--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -1,19 +1,27 @@
 import Employee from "../Employee";
 import "./teams.css";
+import hexToRgba from "hex-to-rgba";
 
 const Teams = (props) => {
+  console.log(props);
   return (
     props.employees.length > 0 && (
       <section
         className="teams"
-        style={{ backgroundColor: props.secondaryColor }}
+        style={{ backgroundColor: hexToRgba(props.team.color, '0.6') }}
       >
+        <input
+          onChange={(event) =>
+            props.changeColor(event.target.value, props.name)
+          }
+          value={props.team.color} type="color" className="input-color"
+        />
         <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
         <div className="employees">
           {props.employees.map((employee) => {
             return (
               <Employee
-                backgroundColor={props.primaryColor}
+                backgroundColor={props.team.color}
                 key={employee.name}
                 name={employee.name}
                 position={employee.position}
